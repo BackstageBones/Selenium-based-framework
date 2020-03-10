71f6d171092b9f5ea1cb919103c6cbd0466c7ee7
BackstageBones <a.miendlarzewski@gmail.com>
2020-03-10 14:34:28 +0100

    finished creating test cases

--- ENV/ninja_trials_page.py
+++ ENV/ninja_trials_page.py
@@ -16,6 +16,11 @@ class NinjaTrialPage(Environment):
         self.riddle_of_secret_anwser_id = "successBanner1"
         self.the_two_merchants_jessica_xpath = "//p[text() = '3000']"
         self.the_two_merchants_bernard_xpath =  "//p[text() = '2000']"
+        self.the_name_of_the_richiest_merchant_id = "r3Input"
+        self.the_two_merchants_anwser_button_id = "r3Butn"
+        self.the_two_merchants_anwser_id = 'successBanner2'
+        self.check_anwsers_id = 'checkButn'
+        self.check_anwsers_output_id ='trialCompleteBanner'
 
     def verify_elements_presence(self) -> bool:
         if self.is_element_present(self.riddle_of_stone_text_input_id):
@@ -35,6 +40,21 @@ class NinjaTrialPage(Environment):
         self.click_element(self.button_riddle_of_secrets_id)
         return self.get_text_by_id(self.riddle_of_secret_anwser_id)
 
-    #def solve_the_two_merchants_riddle(self):
+    def solve_the_riddle_of_two_merchants(self):
+        jessica = self.get_text_by_xpath(self.the_two_merchants_jessica_xpath)
+        bernard = self.get_text_by_xpath(self.the_two_merchants_bernard_xpath)
+        if int(bernard) > int(jessica):
+            self.input_text(self.the_name_of_the_richiest_merchant_id,  'Bernard')
+        else:
+            self.input_text(self.the_name_of_the_richiest_merchant_id, 'Jessica')
+        self.click_element(self.the_two_merchants_anwser_button_id)
+        return self.get_text_by_id(self.the_two_merchants_anwser_id)
+
+    def check_all_the_anwsers(self):
+        self.click_element(self.check_anwsers_id)
+        return self.get_text_by_id(self.check_anwsers_output_id)
+
+
+
 
 
--- ENV/test_training_ground_coverage.py
+++ ENV/test_training_ground_coverage.py
@@ -3,16 +3,31 @@ import pytest
 from ENV.Generic_test_class import GenericTestClass
 
 
-
 class TestClass(GenericTestClass):
 
-    @pytest.mark.usefixtures('env', 'setup_teardown')
+    @pytest.mark.usefixtures('setup_teardown')
     def test_solve_riddle_of_stones(self, env):
         global anwser
         anwser = env.solve_riddle_of_stones('rock')
         assert anwser == 'bamboo'
 
-    @pytest.mark.usefixtures('env', 'setup_teardown')
+    @pytest.mark.usefixtures('setup_teardown')
     def test_solve_riddle_of_secrets(self, env):
-        anwser_check = env.solve_riddle_of_secrets(anwser)
+        secrets_anwser = env.solve_riddle_of_secrets(anwser)
+        assert secrets_anwser == 'Success!'
+
+    @pytest.mark.usefixtures('setup_teardown')
+    def test_solve_the_two_merchants_riddle(self, env):
+        anwser_check = env.solve_the_riddle_of_two_merchants()
         assert anwser_check == 'Success!'
+
+    @pytest.mark.usefixtures('setup_teardown')
+    def test_solve_the_trial_of_stones(self, env):
+        anwser = env.solve_riddle_of_stones('rock')
+        assert anwser == 'bamboo'
+        secrets_anwser = env.solve_riddle_of_secrets(anwser)
+        assert secrets_anwser == 'Success!'
+        anwser_check = env.solve_the_riddle_of_two_merchants()
+        assert anwser_check == 'Success!'
+        finall_anwser = env.check_all_the_anwsers()
+        assert finall_anwser == 'Trial Complete'
