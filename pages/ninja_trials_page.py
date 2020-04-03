a5a4ec467d127addccd7f9dd6d860c070ceaa30a
BackstageBones <a.miendlarzewski@gmail.com>
2020-04-03 18:02:07 +0200

    formated lines'

--- pages/ninja_trials_page.py
+++ pages/ninja_trials_page.py
@@ -8,13 +8,10 @@ class NinjaTrialPage(BasePage):
         self.url = url
         super().__init__(url)
 
-
         self.riddle_of_stone_textbox = BaseElement(self.driver, self.by.ID, "r1Input")
         self.riddle_of_stone_button = BaseElement(self.driver, self.by.ID, "r1Btn")
-
         self.riddle_of_secrets_textbox = BaseElement(self.driver, self.by.ID, "r2Input")
         self.riddle_of_secrets_button = BaseElement(self.driver, self.by.ID, "r2Butn")
-
         self.the_two_merchants_jessica_control = BaseElement(self.driver, self.by.XPATH, "//p[text() = '3000']")
         self.the_two_merchants_bernard_control = BaseElement(self.driver, self.by.XPATH, "//p[text() = '2000']")
         self.the_name_of_the_richiest_merchant_textbox = BaseElement(self.driver, self.by.ID, "r3Input")
@@ -22,25 +19,24 @@ class NinjaTrialPage(BasePage):
 
         self.check_all_anwsers_button = BaseElement(self.driver, self.by.ID, 'checkButn')
 
-
     def solve_riddle_of_stones(self, text):
         self.riddle_of_stone_textbox.send_text(text)
         self.riddle_of_stone_button.click()
         self.riddle_of_stone_anwser_control = BaseElement(
-                                                self.driver,
-                                                self.by.ID,
-                                                "passwordBanner"
-                                                        )
+            self.driver,
+            self.by.ID,
+            "passwordBanner"
+        )
         return self.riddle_of_stone_anwser_control.get_text
 
     def solve_riddle_of_secrets(self, text):
         self.riddle_of_secrets_textbox.send_text(text)
         self.riddle_of_secrets_button.click()
         self.riddle_of_secret_anwser_control = BaseElement(
-                                                self.driver,
-                                                self.by.ID,
-                                                "successBanner1"
-                                                            )
+            self.driver,
+            self.by.ID,
+            "successBanner1"
+        )
         return self.riddle_of_secret_anwser_control.get_text
 
     def solve_the_riddle_of_two_merchants(self):
@@ -52,17 +48,17 @@ class NinjaTrialPage(BasePage):
             self.the_name_of_the_richiest_merchant_textbox.send_text('Jessica')
         self.the_two_merchants_anwser_button.click()
         self.the_two_merchants_anwser_control = BaseElement(
-                                                self.driver,
-                                                self.by.ID,
-                                                'successBanner2'
-                                                            )
+            self.driver,
+            self.by.ID,
+            'successBanner2'
+        )
         return self.the_two_merchants_anwser_control.get_text
 
     def check_all_the_anwsers(self):
         self.check_all_anwsers_button.click()
         self.check_all_anwsers_output_control = BaseElement(
-                                                self.driver,
-                                                self.by.ID,
-                                                'trialCompleteBanner'
-                                                            )
+            self.driver,
+            self.by.ID,
+            'trialCompleteBanner'
+        )
         return self.check_all_anwsers_output_control.get_text
